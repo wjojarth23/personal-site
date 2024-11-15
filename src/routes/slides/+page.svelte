1a85c6f633f914ebb50ed00d61baa203ef13c881
wjojarth23 <wjojarth27@dtechhs.org>
2024-11-15 09:59:33 -0800

    test

--- src/routes/slides/+page.svelte
+++ src/routes/slides/+page.svelte
@@ -1,5 +1,117 @@
 <script>
-  import './styles.scss'
+  import '../styles.scss'
   import { onMount } from 'svelte';
+  import spacedeb1 from '$lib/images/slides/spacedeb1.webp';
+  import spacedeb2 from '$lib/images/slides/spacedeb2.webp';
+  import spacedeb3 from '$lib/images/slides/spacedeb3.webp';
+  import { book, open_book, arrow, emoji, artist, gear, school, brain, oldsites, boring, habitual, morado, postcards, github, instagram, twitter, discord } from '$lib/images/images';
+  
 </script>
-<header>Coming Soon!</header>
+<nav class="navbar">
+  <a href="#" id="title">
+    <h2><span>W</span><span>i</span><span>l</span><span>l</span><span>i</span><span>a</span><span>m</span> <span>J</span><span>o</span><span>J</span><span>a</span><span>r</span><span>t</span><span>h</span></h2>
+  </a>
+  <div id="socials">
+    <a href="https://github.com/wjojarth23" target="_blank" rel="noreferrer" aria-label="Go to William's Github"><img src={github} alt="github logo"/></a>
+    <a href="https://discord.com/users/801117219945578516" target="_blank" rel="noreferrer" aria-label="Go to William's Discord"><img src={discord} alt="discord logo"/></a>
+  </div>
+</nav>
+
+<div class="body">
+<h1 class="line">I use </h1><div class="figmabox"><img class= "figmalogo"src="src/lib/images/icons/figma.svg"/>Figma</div><h1 class="line">, a next generation design tool to create stunning visual effects</h1>
+
+<br/>
+<span>Get started with Figma for Education, and copy one of the decks below!</span>
+<br/>
+<br/>
+<a class="set" href="https://www.figma.com/slides/1EcpzU2zz7mZxmoLrw4uML/AP-Seminar-TMP?node-id=137-159&t=zzW8H72RgUWRY7gA-1">
+<div class="deck">
+<img class="deckimg bot" src={spacedeb1}/>
+<img class="deckimg mid" src={spacedeb2}/>
+<img class="deckimg top" src={spacedeb3}/>
+
+</div>
+<h1>Black and Colorful</h1>
+</a>
+
+</div>
+<style>
+@mixin fade-up($duration, $delay) {
+  animation-name: fade-up;
+  animation-duration: $duration;
+  animation-delay: $delay;
+  animation-timing-function: cubic-bezier(0.33, 1, 0.68, 1);
+  animation-fill-mode: forwards;
+  transform: translateY(1em);
+  opacity: 0;
+}
+@keyframes fade-up {
+  from {
+    transform: translateY(1em);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+h1{
+  font-family: articulat-cf, sans-serif;
+  font-size:40px;
+  display:inline-block;
+}
+.mid{
+  opacity:80%;
+}
+a{
+  color:black;
+}
+.top{
+  opacity:100%;
+}
+.bot{
+  opacity:40%;
+}
+.deckimg{
+  width:500px;
+  border-radius:25px;
+  display:inline-block;
+  margin-left:-460px;
+  transform:translate(460px,0px);
+  transition: 2s;
+}
+.deckimg:hover{
+  width:500px;
+  border-radius:25px;
+  display:inline-block;
+  margin-left:-200px;
+  transform:translate(200px,0px);
+  opacity:100% !important;
+}
+.body{
+  margin-top:160px;
+  margin-left:160px;
+  margin-right:160px;
+  @include fade-up(1.5s, 2.5s);
+}
+.deck{
+
+}
+.figmalogo{
+  height:40px;
+  transform: translate(0px, 5px);
+  margin-right:10px;
+  @include fade-up(1.5s, 2.5s);
+  transition: 0.2s ease-in-out;
+}
+.figmabox{
+  background:#E0E0E0;
+  padding:6px;
+  margin-left:10px;
+  font-weight:700;
+  font-size:40px;
+  border-radius: 10px;
+  width:170px;
+  display:inline-block;
+}
+</style>
